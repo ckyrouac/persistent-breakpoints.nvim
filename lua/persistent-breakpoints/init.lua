ec24664ff912a702edc1cd526579886cb255029c
ckyrouac <ckyrouac@redhat.com>
2024-02-22 14:11:34 -0500

    Fix loading all buffers with breakpoints

--- lua/persistent-breakpoints/init.lua
+++ lua/persistent-breakpoints/init.lua
@@ -1,40 +1,42 @@
-local utils = require("persistent-breakpoints.utils")
-local inmem_bps = require("persistent-breakpoints.inmemory")
 local cfg = require("persistent-breakpoints.config")
+local inmem_bps = require("persistent-breakpoints.inmemory")
+local utils = require("persistent-breakpoints.utils")
 
 local M = {}
 
 M.setup = function(_cfg)
-	_cfg = _cfg or {}
-	local tmp_config = vim.tbl_deep_extend("force", cfg, _cfg)
-	for key, val in pairs(tmp_config) do
-		cfg[key] = val
-	end
-	inmem_bps.bps = utils.load_bps(utils.get_bps_path()) -- {'filename':breakpoints_table}
+  _cfg = _cfg or {}
+  local tmp_config = vim.tbl_deep_extend("force", cfg, _cfg)
+  for key, val in pairs(tmp_config) do
+    cfg[key] = val
+  end
+  inmem_bps.bps = utils.load_bps(utils.get_bps_path()) -- {'filename':breakpoints_table}
 
-	-- load all buffers with a breakpoint set
-	vim.schedule(function ()
-		if vim.bo.filetype ~= "gitcommit" then
-			for file in pairs(inmem_bps.bps) do
-				vim.cmd.edit(file)
-			end
-		end
-	end)
+  -- load all buffers with a breakpoint set
+  vim.schedule(function()
+    if vim.bo.filetype ~= "gitcommit" then
+      for file in pairs(inmem_bps.bps) do
+        local bufnr = vim.fn.bufadd(file)
+        vim.fn.bufload(bufnr)
+        vim.fn.setbufvar(bufnr, "&buflisted", true)
+      end
+    end
+  end)
 
-	utils.create_path(cfg.save_dir)
-	if tmp_config.load_breakpoints_event ~= nil then
-		local aug = vim.api.nvim_create_augroup("persistent-breakpoints-load-breakpoint", {
-			clear = true,
-		})
-		vim.api.nvim_create_autocmd(
-			tmp_config.load_breakpoints_event,
-			{ callback = require("persistent-breakpoints.api").load_breakpoints, group = aug }
-		)
-		vim.api.nvim_create_autocmd(
-			{'DirChanged'},
-			{ callback = require("persistent-breakpoints.api").reload_breakpoints, group = aug }
-		)
-	end
+  utils.create_path(cfg.save_dir)
+  if tmp_config.load_breakpoints_event ~= nil then
+    local aug = vim.api.nvim_create_augroup("persistent-breakpoints-load-breakpoint", {
+      clear = true,
+    })
+    vim.api.nvim_create_autocmd(
+      tmp_config.load_breakpoints_event,
+      { callback = require("persistent-breakpoints.api").load_breakpoints, group = aug }
+    )
+    vim.api.nvim_create_autocmd(
+      { "DirChanged" },
+      { callback = require("persistent-breakpoints.api").reload_breakpoints, group = aug }
+    )
+  end
 end
 
 return M
